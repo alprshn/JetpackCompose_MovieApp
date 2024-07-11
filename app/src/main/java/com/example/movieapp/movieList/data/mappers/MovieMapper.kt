072beb2601ea0b5833633bfbdac257c7e9441797
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-11 16:02:45 +0300

    Added: MovieEntity.toMovie()

--- app/src/main/java/com/example/movieapp/movieList/data/mappers/MovieMapper.kt
+++ app/src/main/java/com/example/movieapp/movieList/data/mappers/MovieMapper.kt
@@ -1,4 +1,23 @@
 package com.example.movieapp.movieList.data.mappers
 
-class MovieMapper {
+import com.example.movieapp.movieList.data.local.movie.MovieEntity
+import com.example.movieapp.movieList.domain.model.Movie
+
+fun MovieEntity.toMovie(): Movie {
+    return Movie(
+        adult = adult,
+        backdrop_path = backdrop_path,
+        genre_ids = genre_ids,
+        id = id,
+        original_language = original_language,
+        original_title = original_title,
+        overview = overview,
+        popularity = popularity,
+        poster_path = poster_path,
+        release_date = release_date,
+        title = title,
+        video = video,
+        vote_average = vote_average,
+        vote_count = vote_count,
+    )
 }
