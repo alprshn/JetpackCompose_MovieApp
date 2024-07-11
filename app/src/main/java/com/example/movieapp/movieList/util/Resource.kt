41c88e003f253882b5baf03d0614d19323d65549
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-11 14:01:43 +0300

    Added: Resource

--- app/src/main/java/com/example/movieapp/movieList/util/Resource.kt
+++ app/src/main/java/com/example/movieapp/movieList/util/Resource.kt
@@ -1,5 +1,11 @@
 package com.example.movieapp.movieList.util
 
-class Resource {
 
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T?):Resource<T>(data)
+    class Error<T>(message: String, data: T? = null):Resource<T>(data,message)
+    class Loading<T>(val isLoading: Boolean = true):Resource<T>( null)
 }
