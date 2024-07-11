cb3f59768dc64bb8e0ebee5b5e7accc8ec4f2e27
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-11 11:59:36 +0300

    Edit: MovieApi

--- app/src/main/java/com/example/movieapp/movieList/data/remote/MovieApi.kt
+++ app/src/main/java/com/example/movieapp/movieList/data/remote/MovieApi.kt
@@ -1,5 +1,20 @@
 package com.example.movieapp.movieList.data.remote
 
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
 interface MovieApi {
+
+    @GET("search/movie")
+    suspend fun searchMovie(
+        @Query("include_adult") includeAdult: Boolean = false,
+        @Query("page") page: Int = 1,
+        @Header("Authorization") authorization: String
+    )
+
+    companion object {
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+    }
 
 }
