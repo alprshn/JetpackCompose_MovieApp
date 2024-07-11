774b5149f8e69a34bff02b6f176cc42871db313b
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-11 12:46:51 +0300

    Added: Domain Package

--- app/src/main/java/com/example/movieapp/movieList/data/local/movie/MovieDatabase.kt
+++ app/src/main/java/com/example/movieapp/movieList/data/local/movie/MovieDatabase.kt
@@ -1,11 +1,12 @@
 package com.example.movieapp.movieList.data.local.movie
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(
     entities = [MovieEntity::class],
     version = 1
 )
-abstract class MovieDatabase {
-
+abstract class MovieDatabase: RoomDatabase() {
+    abstract val movieDao: MovieDao
 }
---
+++ app/src/main/java/com/example/movieapp/movieList/domain/repository/MovieListRepository.kt
@@ -0,0 +1,4 @@
+package com.example.movieapp.movieList.domain.repository
+
+class MovieListRepository {
+}
