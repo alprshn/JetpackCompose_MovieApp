7c2cd631f187068812405b3cbfa8fd2ebcc7ebef
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-13 14:56:27 +0300

    Edit: FirebaseAuthState

--- app/src/main/java/com/example/movieapp/movieList/domain/use_cases/FirebaseAuthState.kt
+++ app/src/main/java/com/example/movieapp/movieList/domain/use_cases/FirebaseAuthState.kt
@@ -4,4 +4,5 @@ import com.example.movieapp.movieList.domain.repository.AuthenticationRepository
 import javax.inject.Inject
 
 class FirebaseAuthState @Inject constructor(private val repository: AuthenticationRepository){
+    operator fun invoke() = repository.getFirebaseAuthState()
 }
