95cfcabe3eb8c1a4ea3b241f0aa258a6cb692f65
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-13 14:55:27 +0300

    Edit: FirebaseSignOut

--- app/src/main/java/com/example/movieapp/movieList/domain/use_cases/FirebaseSignOut.kt
+++ app/src/main/java/com/example/movieapp/movieList/domain/use_cases/FirebaseSignOut.kt
@@ -4,4 +4,5 @@ import com.example.movieapp.movieList.domain.repository.AuthenticationRepository
 import javax.inject.Inject
 
 class FirebaseSignOut @Inject constructor(private val repository: AuthenticationRepository) {
+    operator fun invoke() = repository.firebaseSignOut()
 }
