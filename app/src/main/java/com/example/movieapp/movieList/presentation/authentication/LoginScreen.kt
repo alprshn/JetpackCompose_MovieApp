19a8005c26f68e014bee8b6ea14d38381fdb193e
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-14 09:48:37 +0300

    Edit: LoginScreen

--- app/src/main/java/com/example/movieapp/movieList/presentation/authentication/LoginScreen.kt
+++ app/src/main/java/com/example/movieapp/movieList/presentation/authentication/LoginScreen.kt
@@ -1,6 +1,7 @@
 package com.example.movieapp.movieList.presentation.authentication
 
 import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,11 +20,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.movieapp.movieList.presentation.Toast
 import com.example.movieapp.movieList.util.Resource
 import com.example.movieapp.movieList.util.Screens
 
@@ -79,13 +82,22 @@ fun LoginScreen(navController: NavHostController, viewModel: AuthenticationViewM
                                     inclusive = true
                                 }
                             }
+                        } else {
+                            Toast(message = "Sign In Failed")
                         }
                     }
 
-                    is Resource.Error -> TODO()
-                    Log.e("")
+                    is Resource.Error -> {
+                        Toast(message = response.message.toString())
+                    }
                 }
             }
+            Text(text = "New User? Sign Up", color = Color.Blue, modifier = Modifier.padding(8.dp)
+                .clickable {
+                    navController.navigate(Screens.SignUpScreen.route) {
+                       launchSingleTop = true
+                    }
+                })
         }
 
     }
