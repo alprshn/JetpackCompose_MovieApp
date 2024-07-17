9809b9edfcd40a521f726d131c1819bb2d815374
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-17 10:58:48 +0300

    Added: WatchListScreen.kt

--- app/src/main/java/com/example/movieapp/movieList/presentation/BottomNavigationMenu.kt
+++ app/src/main/java/com/example/movieapp/movieList/presentation/BottomNavigationMenu.kt
@@ -2,12 +2,16 @@ package com.example.movieapp.movieList.presentation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.movieapp.movieList.util.Screens
 
 enum class BottomNavigationMenu(val icon: ImageVector, val route: Screens) {
-    FAVORITES(Icons.Default.FavoriteBorder, Screens.FavoritesScreen)
+    FAVORITES(Icons.Default.FavoriteBorder, Screens.FavoritesScreen),
+    SEARCH(Icons.Default.Search, Screens.SearchScreen),
+    WATCHLIST(Icons.Default.Menu, Screens.WatchListScreen)
 }
 
 @Composable
---
+++ app/src/main/java/com/example/movieapp/movieList/presentation/main/WatchListScreen.kt
@@ -0,0 +1,7 @@
+package com.example.movieapp.movieList.presentation.main
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WatchListScreen() {
+}
