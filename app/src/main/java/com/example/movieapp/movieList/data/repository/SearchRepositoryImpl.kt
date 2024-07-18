d1c5705b135d685c81f37f64bc0de8f01492dc8c
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-18 23:36:14 +0300

    Edit: SearchRepository

--- app/src/main/java/com/example/movieapp/movieList/data/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/com/example/movieapp/movieList/data/repository/SearchRepositoryImpl.kt
@@ -7,6 +7,7 @@ import com.example.movieapp.movieList.data.remote.MovieApi
 import com.example.movieapp.movieList.data.remote.respond.SearchMovie
 import com.example.movieapp.movieList.domain.repository.MovieListRepository
 import com.example.movieapp.movieList.domain.repository.SearchRepository
+import com.example.movieapp.movieList.util.Constants
 import com.example.movieapp.movieList.util.Resource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -17,20 +18,12 @@ import javax.inject.Inject
 class SearchRepositoryImpl @Inject constructor(
     private val movieApi: MovieApi,
 ) : SearchRepository {
-    override suspend fun searchQueries(query: String): Flow<Resource<SearchMovie>> {
-        return flow {
-            emit(Resource.Loading(true))
-            try {
-                val authorization = "Bearer YOUR_API_KEY"
-                movieApi.searchMovie(query, authorization)
-            } catch (e: IOException) {
-                emit(Resource.Error("Couldn't load data due to network failure"))
-            } catch (e: HttpException) {
-                emit(Resource.Error("Couldn't load data due to server response error"))
-            } catch (e: Exception) {
-                emit(Resource.Error("An unexpected error occurred"))
-            }
-            emit(Resource.Loading(false))
+    override suspend fun searchQueries(query: String): Flow<Resource<SearchMovie>> { 
+        try {
+            val result = movieApi.searchMovie(query = query, authorization = Constants.API_KEY)
+            Resource.Success(data = result)
+        } catch (e:Exception){
+            Resource.Error(message = e.message.toString())
         }
     }
 }
--- app/src/main/java/com/example/movieapp/movieList/util/Constants.kt
+++ app/src/main/java/com/example/movieapp/movieList/util/Constants.kt
@@ -2,4 +2,5 @@ package com.example.movieapp.movieList.util
 
 object Constants {
     const val COLLECTION_NAME_USERS = "users"
+    const val API_KEY = "movies"
 }
