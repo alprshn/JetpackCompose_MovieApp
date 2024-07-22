b1156415567795f9b7607caadcb53a5de09749f5
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-23 00:49:52 +0300

    Edit : DetailRepository.kt

--- app/src/main/java/com/example/movieapp/movieList/domain/repository/DetailRepository.kt
+++ app/src/main/java/com/example/movieapp/movieList/domain/repository/DetailRepository.kt
@@ -3,9 +3,10 @@ package com.example.movieapp.movieList.domain.repository
 import com.example.movieapp.movieList.data.local.movie.FirebaseMovieEntity
 import com.example.movieapp.movieList.data.local.movie.MovieEntity
 import com.example.movieapp.movieList.data.remote.respond.Result
+import com.example.movieapp.movieList.data.remote.respond.SearchMovie
 
 interface DetailRepository {
-    suspend fun getMovieFromApi(movieId: Int): Result
+    suspend fun getMovieFromApi(movieId: Int, authorization: String): SearchMovie?
     suspend fun getMovieFromRoom(movieId: Int, userId: String): MovieEntity?
     suspend fun getMovieFromFirebase(movieId: Int, userId: String): FirebaseMovieEntity?
     suspend fun updateFavoriteStatus(movieId: Int, userId: String, isFavorite: Boolean)
