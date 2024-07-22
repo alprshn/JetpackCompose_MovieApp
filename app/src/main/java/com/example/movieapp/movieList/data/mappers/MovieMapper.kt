ba7eaca8aa5af7556ddc5ee855a21292171679ea
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-22 20:18:41 +0300

    Edit: MovieMapper.kt

--- app/src/main/java/com/example/movieapp/movieList/data/mappers/MovieMapper.kt
+++ app/src/main/java/com/example/movieapp/movieList/data/mappers/MovieMapper.kt
@@ -1,53 +1,75 @@
 package com.example.movieapp.movieList.data.mappers
 
+import com.example.movieapp.movieList.data.local.movie.FirebaseMovieEntity
 import com.example.movieapp.movieList.data.local.movie.MovieEntity
 import com.example.movieapp.movieList.data.remote.respond.Result
 import com.example.movieapp.movieList.domain.model.Movie
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
-fun Result.toMovieEntity(isFavorite: Boolean = false): MovieEntity {
-    return MovieEntity(
-        adult = adult ?: false,
-        backdrop_path = backdrop_path ?: "",
-        id = id ?: -1,
-        original_language = original_language ?: "",
-        original_title = original_title ?: "",
-        overview = overview ?: "",
-        popularity = popularity ?: 0.0,
-        poster_path = poster_path ?: "",
-        release_date = release_date ?: "",
-        title = title ?: "",
-        video = video ?: false,
-        vote_average = vote_average ?: 0.0,
-        vote_count = vote_count ?: 0,
-        isFavorite = isFavorite,
-        genre_ids = try {
-            genre_ids?.joinToString(",") ?: "-1,-2"
-        } catch (e: Exception) {
-            "-1,-2"
-        },
-        userId = original_language ?: "",
+
+fun fromJsonToList(json: String): List<Int> {
+    val type = object : TypeToken<List<Int>>() {}.type
+    return Gson().fromJson(json, type)
+}
+
+fun fromListToJson(list: List<Int>): String {
+    return Gson().toJson(list)
+}
+
+fun Result.toDomainModel(): Movie {
+    return Movie(
+        adult = this.adult,
+        backdrop_path = this.backdrop_path,
+        genre_ids = this.genre_ids,
+        id = this.id,
+        original_language = this.original_language,
+        original_title = this.original_title,
+        overview = this.overview,
+        popularity = this.popularity,
+        poster_path = this.poster_path,
+        release_date = this.release_date,
+        title = this.title,
+        video = this.video,
+        vote_average = this.vote_average,
+        vote_count = this.vote_count
     )
 }
 
-fun MovieEntity.toMovie(): Movie {
+fun MovieEntity.toDomainModel(): Movie {
     return Movie(
-        adult = adult,
-        backdrop_path = backdrop_path,
-        id = id,
-        original_language = original_language,
-        original_title = original_title,
-        overview = overview,
-        popularity = popularity,
-        poster_path = poster_path,
-        release_date = release_date,
-        title = title,
-        video = video,
-        vote_average = vote_average,
-        vote_count = vote_count,
-        genre_ids = try {
-            genre_ids.toString().split(",").map { it.toInt() }
-        }catch (e: Exception){
-            listOf(-1,-2)
-        }
+        adult = this.adult,
+        backdrop_path = this.backdrop_path,
+        genre_ids = fromJsonToList(this.genre_ids),
+        id = this.id,
+        original_language = this.original_language,
+        original_title = this.original_title,
+        overview = this.overview,
+        popularity = this.popularity,
+        poster_path = this.poster_path,
+        release_date = this.release_date,
+        title = this.title,
+        video = this.video,
+        vote_average = this.vote_average,
+        vote_count = this.vote_count
+    )
+}
+
+fun FirebaseMovieEntity.toDomainModel(): Movie {
+    return Movie(
+        adult = this.adult,
+        backdrop_path = this.backdrop_path ?: "",
+        genre_ids = fromJsonToList(this.genre_ids ?: ""),
+        id = this.id ?: 0,
+        original_language = this.original_language ?: "",
+        original_title = this.original_title ?: "",
+        overview = this.overview ?: "",
+        popularity = this.popularity,
+        poster_path = this.poster_path ?: "",
+        release_date = this.release_date ?: "",
+        title = this.title ?: "",
+        video = this.video,
+        vote_average = this.vote_average ?: 0.0,
+        vote_count = this.vote_count ?: 0
     )
 }
