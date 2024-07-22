4d3afd8e6c057b6c322b76cb3acb85c45f653fff
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-23 00:58:39 +0300

    Edit: Deleted Extra Dependencies

--- app/src/main/java/com/example/movieapp/movieList/data/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/com/example/movieapp/movieList/data/repository/SearchRepositoryImpl.kt
@@ -3,19 +3,10 @@ package com.example.movieapp.movieList.data.repository
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import com.example.movieapp.movieList.data.local.movie.MovieDatabase
-import com.example.movieapp.movieList.data.mappers.toMovie
-import com.example.movieapp.movieList.data.mappers.toMovieEntity
 import com.example.movieapp.movieList.data.remote.MovieApi
 import com.example.movieapp.movieList.data.remote.respond.Result
-import com.example.movieapp.movieList.data.remote.respond.SearchMovie
 import com.example.movieapp.movieList.domain.repository.SearchRepository
-import com.example.movieapp.movieList.util.Constants
-import com.example.movieapp.movieList.util.Resource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import retrofit2.HttpException
-import java.io.IOException
 import javax.inject.Inject
 
 class SearchRepositoryImpl @Inject constructor(
