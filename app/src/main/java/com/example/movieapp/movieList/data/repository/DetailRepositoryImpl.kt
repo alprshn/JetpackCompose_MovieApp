b4d79cf25d4eef1d04312ff1b2e57542bab217b1
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-22 20:34:17 +0300

    Added: DetailRepositoryImpl.kt

---
+++ app/src/main/java/com/example/movieapp/movieList/data/repository/DetailRepositoryImpl.kt
@@ -0,0 +1,67 @@
+package com.example.movieapp.movieList.data.repository
+
+import com.example.movieapp.movieList.data.local.movie.FirebaseMovieEntity
+import com.example.movieapp.movieList.data.local.movie.MovieDao
+import com.example.movieapp.movieList.data.local.movie.MovieEntity
+import com.example.movieapp.movieList.data.remote.MovieApi
+import com.example.movieapp.movieList.data.remote.respond.Result
+import com.example.movieapp.movieList.domain.repository.DetailRepository
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class DetailRepositoryImpl @Inject constructor(
+    private val apiService: MovieApi,
+    private val movieDao: MovieDao,
+    private val firestore: FirebaseFirestore
+) : DetailRepository {
+    override suspend fun getMovieFromApi(movieId: Int): Result {
+        apiService.searchMovie(movieId)
+    }
+
+    override suspend fun getMovieFromRoom(movieId: Int, userId: String): MovieEntity? {
+        return movieDao.getMovieById(movieId, userId)
+
+    }
+
+    override suspend fun getMovieFromFirebase(movieId: Int, userId: String): FirebaseMovieEntity? {
+        return withContext(IO) {
+            val documentSnapshot = firestore.collection("watchlist")
+                .document(userId)
+                .collection("watchlistMovies")
+                .document(movieId.toString())
+                .get()
+                .await()
+            documentSnapshot.toObject<FirebaseMovieEntity>()
+        }
+    }
+
+    override suspend fun updateFavoriteStatus(movieId: Int, userId: String, isFavorite: Boolean) {
+        movieDao.updateFavoriteStatus(movieId, userId, isFavorite)
+        if (!isFavorite) {
+            movieDao.deleteMovie(movieId, userId)
+        }
+    }
+
+    override suspend fun updateWatchlistStatus(
+        movieId: Int,
+        userId: String,
+        addedToWatchlist: Boolean
+    ) {
+        withContext(IO) {
+            val movieRef = firestore.collection("watchlist")
+                .document(userId)
+                .collection("watchlistMovies")
+                .document(movieId.toString())
+            if (addedToWatchlist) {
+                movieRef.update("addedToWatchlist", true).await()
+            } else {
+                movieRef.delete().await()
+            }
+        }
+    }
+
+}
