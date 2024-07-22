c3bec24ec44f404ed793efc5389c560672848e04
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-23 00:51:42 +0300

    Edit: Added TypeConverters

--- app/src/main/java/com/example/movieapp/movieList/data/local/movie/MovieDatabase.kt
+++ app/src/main/java/com/example/movieapp/movieList/data/local/movie/MovieDatabase.kt
@@ -1,14 +1,16 @@
 package com.example.movieapp.movieList.data.local.movie
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.movieapp.movieList.util.Converters
 
 @Database(
     entities = [MovieEntity::class],
     version = 2,
 )
+@TypeConverters(Converters::class)
+
 abstract class MovieDatabase: RoomDatabase() {
     abstract val movieDao: MovieDao
 }
