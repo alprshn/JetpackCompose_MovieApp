7bad8b38bdff3a8325cd3a342989396275e17d10
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-23 00:42:19 +0300

    Added:Converters.kt

---
+++ app/src/main/java/com/example/movieapp/movieList/util/Converters.kt
@@ -0,0 +1,18 @@
+package com.example.movieapp.movieList.util
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class Converters {
+    @TypeConverter
+    fun fromString(value: String): List<Int> {
+        val listType = object : TypeToken<List<Int>>() {}.type
+        return Gson().fromJson(value, listType)
+    }
+
+    @TypeConverter
+    fun fromList(list: List<Int>): String {
+        return Gson().toJson(list)
+    }
+}
