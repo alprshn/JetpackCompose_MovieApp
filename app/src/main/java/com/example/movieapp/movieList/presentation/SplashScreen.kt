62889ad4110d45edc2b1de1b1c78af38d3e2bf83
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-23 16:56:45 +0300

    Edit: isUserAuthenticated

--- app/src/main/java/com/example/movieapp/movieList/presentation/SplashScreen.kt
+++ app/src/main/java/com/example/movieapp/movieList/presentation/SplashScreen.kt
@@ -1,21 +1,25 @@
 package com.example.movieapp.movieList.presentation
 
+import android.util.Log
 import android.view.animation.OvershootInterpolator
 import androidx.compose.animation.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.movieapp.movieList.util.Screens
 import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen(navController: NavController) {
+fun SplashScreen(navController: NavController, viewModel: AuthenticationViewModel = hiltViewModel()) {
 
-    //val authValue = authViewModel.isUserAuthenticated
+    val authValue by viewModel.isUserAuthenticated.collectAsState()
 
     val scale = remember { androidx.compose.animation.core.Animatable(0f) }
 
@@ -28,20 +32,21 @@ fun SplashScreen(navController: NavController) {
                     OvershootInterpolator(2f).getInterpolation(it)
                 })
         )
-        delay(3000)
-
-//        if (authValue) {
-//            navController.navigate(Screens.SearchScreen.route) {
-//                popUpTo(Screens.SplashScreen.route) {
-//                    inclusive = true
-//                }
-//            }
-//        } else {
+        delay(1000)
+        Log.e("authValue", authValue.toString())
+        if (authValue) {
+            navController.navigate(Screens.SearchScreen.route) {
+                popUpTo(Screens.SplashScreen.route) {
+                    inclusive = true
+                }
+            }
+        } else {
             navController.navigate(Screens.LoginScreen.route) {
                 popUpTo(Screens.SplashScreen.route) {
                     inclusive = true
                 }
             }
 
+        }
     }
 }
