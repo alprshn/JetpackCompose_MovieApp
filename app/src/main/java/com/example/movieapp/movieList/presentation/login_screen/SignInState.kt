99733a586fb544708a8a0594c6bd4f0a915d3b7f
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-23 16:52:32 +0300

    Added: Deleted Extra ViewModel and States

--- app/src/main/java/com/example/movieapp/movieList/presentation/login_screen/SignInState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.movieapp.movieList.presentation.login_screen
-
-data class SignInState(
-    val isLoading: Boolean = false,
-    val isSuccess: String? = "",
-    val isError: String = ""
-)
--- app/src/main/java/com/example/movieapp/movieList/presentation/signup_screen/SignUpState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.movieapp.movieList.presentation.signup_screen
-
-data class SignUpState(
-    val isLoading: Boolean = false,
-    val isSuccess: String? = "",
-    val isError: String = ""
-)
