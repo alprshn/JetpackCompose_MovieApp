dd75422f6c018e9ecf054541958ac744db3c4b16
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-24 00:14:22 +0300

    Added: Added Lottie Animation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -124,6 +124,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    //lottie
+    implementation(libs.lottie)
+    implementation(libs.lottie.compose)
+
 
 }
 kapt {
--- app/src/main/java/com/example/movieapp/movieList/presentation/SplashScreen.kt
+++ app/src/main/java/com/example/movieapp/movieList/presentation/SplashScreen.kt
@@ -5,14 +5,22 @@ import android.view.animation.OvershootInterpolator
 import androidx.compose.animation.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.movieapp.R
 import com.example.movieapp.movieList.util.Screens
 import kotlinx.coroutines.delay
 
@@ -23,6 +31,17 @@ fun SplashScreen(navController: NavController, viewModel: AuthenticationViewMode
 
     val scale = remember { androidx.compose.animation.core.Animatable(0f) }
 
+    val composition by rememberLottieComposition(spec = LottieCompositionSpec.Url("https://lottie.host/7b919bcd-15c2-4360-98fb-97ded08c9f28/QY6HPNAWcp.json"))
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        LottieAnimation(
+            composition = composition,
+            iterations = LottieConstants.IterateForever,
+        )
+    }
     LaunchedEffect(key1 = true) {
         scale.animateTo(
             targetValue = 0.5f,
