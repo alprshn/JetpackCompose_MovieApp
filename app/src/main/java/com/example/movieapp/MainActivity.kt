8a8178acad65bbff92653beb894d5426acb88e75
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-07-27 15:39:17 +0300

    Edit: Added Settings Screen

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.movieapp.movieList.data.remote.api.response.Result
+import com.example.movieapp.movieList.presentation.SettingsScreen
 import com.example.movieapp.movieList.presentation.navigation.BottomNavigation
 import com.example.movieapp.movieList.presentation.search_screen.SearchScreen
 import com.example.movieapp.movieList.presentation.splash_screen.SplashScreen
