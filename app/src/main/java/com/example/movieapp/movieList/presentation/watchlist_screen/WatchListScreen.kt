310b93b5cd767b431a5834e5caebe71611a440b2
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-08-01 22:36:46 +0300

    Edit: Made WatchListScreen Modular

--- app/src/main/java/com/example/movieapp/movieList/presentation/watchlist_screen/WatchListScreen.kt
+++ app/src/main/java/com/example/movieapp/movieList/presentation/watchlist_screen/WatchListScreen.kt
@@ -47,11 +47,14 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import coil.compose.rememberImagePainter
 import com.example.movieapp.R
+import com.example.movieapp.movieList.presentation.components.MovieCard
 import com.example.movieapp.movieList.presentation.viewmodel.MainViewModel
 import com.example.movieapp.movieList.util.Screens
 import com.example.movieapp.ui.theme.backgroundColor
+import com.example.movieapp.ui.theme.darkGreyColor
 import com.example.movieapp.ui.theme.latoFontFamily
 import com.example.movieapp.ui.theme.starColor
+import com.example.movieapp.ui.theme.whiteColor
 import com.google.gson.Gson
 
 
@@ -77,7 +80,7 @@ fun WatchListScreen(viewModel: MainViewModel = hiltViewModel(), navController: N
         ) {
             Text(
                 text = stringResource(id = R.string.watchlist),
-                color = Color.White,
+                color = whiteColor,
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Normal,
                 fontFamily = latoFontFamily,
@@ -90,124 +93,7 @@ fun WatchListScreen(viewModel: MainViewModel = hiltViewModel(), navController: N
             ) {
                 items(watchlistMovies) { movie ->
                     val movie = MovieMapper().firestoreMapToResult(movie)
-                    Card(
-                        colors = CardDefaults.cardColors(containerColor = backgroundColor),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(backgroundColor)
-                            .padding(vertical = 4.dp)
-                            .clickable {
-                                val movieJson = Uri.encode(Gson().toJson(movie))
-                                Log.e("MovieJson", movieJson)
-                                navController.navigate(Screens.DetailScreen.route + "/$movieJson")
-                            }
-                    ) {
-                        Row(
-                            modifier = Modifier
-                                .padding(8.dp)
-                                .height(200.dp)
-                                .background(backgroundColor)
-                        ) {
-                            Box(
-                                modifier = Modifier
-                                    .clip(RoundedCornerShape(20.dp))
-                                    .width(130.dp)
-                                    .background(Color.DarkGray)
-                            ) {
-                                Image(
-                                    painter = rememberImagePainter(data = "https://image.tmdb.org/t/p/original${movie.poster_path}"),
-                                    contentScale = ContentScale.FillHeight,
-                                    contentDescription = null,
-                                    modifier = Modifier
-                                        .fillMaxHeight()
-                                )
-                            }
-
-
-                            Column(
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .background(backgroundColor)
-                                    .padding(start = 10.dp, top = 2.dp)
-                            ) {
-                                Text(
-                                    text = movie.title,
-                                    fontWeight = FontWeight.Bold,
-                                    color = Color.White,
-                                    modifier = Modifier.padding(vertical = 2.dp),
-                                    fontSize = 18.sp,
-                                    fontFamily = latoFontFamily
-                                )
-                                Row(
-                                    modifier = Modifier.padding(vertical = 2.dp)
-                                ) {
-                                    Icon(
-                                        modifier = Modifier
-                                            .padding(end = 6.dp)
-                                            .size(23.dp),
-                                        tint = starColor,
-                                        imageVector = Icons.Outlined.StarBorder,
-                                        contentDescription = null
-                                    )
-                                    Text(
-                                        text = String.format("%.1f", movie.vote_average),
-                                        color = starColor,
-                                        fontSize = 15.sp,
-                                        fontFamily = latoFontFamily
-                                    )
-                                }
-
-                                Row(
-                                    modifier = Modifier.padding(vertical = 2.dp)
-                                ) {
-                                    Icon(
-                                        modifier = Modifier
-                                            .padding(end = 6.dp)
-                                            .size(23.dp),
-                                        imageVector = Icons.Outlined.ConfirmationNumber,
-                                        contentDescription = null,
-                                        tint = Color.White,
-
-                                        )
-                                    Text(
-                                        text = movie.getGenreIds()
-                                            .mapNotNull { id -> viewModel.genres.find { it.id == id }?.name }
-                                            .joinToString(", "),
-                                        color = Color.White,
-                                        fontSize = 15.sp,
-                                        fontFamily = latoFontFamily
-                                    )
-                                }
-
-                                Row(
-                                    modifier = Modifier.padding(vertical = 2.dp)
-                                ) {
-                                    Icon(
-                                        modifier = Modifier
-                                            .padding(end = 6.dp)
-                                            .size(23.dp),
-                                        tint = Color.White,
-                                        imageVector = Icons.Outlined.CalendarToday,
-                                        contentDescription = null
-                                    )
-                                    val releaseYear =
-                                        if (movie.release_date.isNullOrEmpty() || movie.release_date.length < 4) {
-                                            "Unknown"
-                                        } else {
-                                            movie.release_date.substring(0, 4)
-                                        }
-                                    Text(
-                                        text = releaseYear,
-                                        color = Color.White,
-                                        fontSize = 15.sp,
-                                        fontFamily = latoFontFamily
-                                    )
-                                }
-
-
-                            }
-                        }
-                    }
+                    MovieCard(navController = navController, movie = movie)
                 }
             }
         }
