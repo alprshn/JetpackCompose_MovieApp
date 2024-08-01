aea5d2fbea3f605f3c4213db5f25050a03c5ddbb
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-08-01 22:43:22 +0300

    Edit: Genre

--- app/src/main/java/com/example/movieapp/movieList/presentation/components/MovieCardDetails.kt
+++ app/src/main/java/com/example/movieapp/movieList/presentation/components/MovieCardDetails.kt
@@ -14,14 +14,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.movieapp.movieList.data.remote.api.response.search_data.Result
+import com.example.movieapp.movieList.presentation.viewmodel.MainViewModel
 import com.example.movieapp.ui.theme.backgroundColor
 import com.example.movieapp.ui.theme.latoFontFamily
 import com.example.movieapp.ui.theme.starColor
 import com.example.movieapp.ui.theme.whiteColor
 
 @Composable
-fun MovieCardDetails(movie: Result) {
+fun MovieCardDetails(movie: Result, viewModel: MainViewModel = hiltViewModel()) {
+
     Column(
         modifier = Modifier
             .fillMaxSize(
@@ -38,7 +41,9 @@ fun MovieCardDetails(movie: Result) {
             fontFamily = latoFontFamily
         )
         MovieCardItems(icon = Icons.Outlined.StarBorder, text = String.format("%.1f", movie.vote_average), color = starColor)
-        MovieCardItems(icon = Icons.Outlined.ConfirmationNumber, text = movie.getGenreIds().joinToString(", "), color = whiteColor)
+        MovieCardItems(icon = Icons.Outlined.ConfirmationNumber, text = movie.getGenreIds()
+            .mapNotNull { id -> viewModel.genres.find { it.id == id }?.name }
+            .joinToString(", "), color = whiteColor)
         val releaseYear =
             if (movie.release_date.isNullOrEmpty() || movie.release_date.length < 4) {
                 "Unknown"
