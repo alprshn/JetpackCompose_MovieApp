fc0d3c44da502759f9a4e18376357ae8deaf5446
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-08-06 18:44:51 +0300

    Added: MovieButton for Composable

---
+++ app/src/main/java/com/example/movieapp/movieList/presentation/components/MovieButton.kt
@@ -0,0 +1,52 @@
+package com.example.movieapp.movieList.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.movieapp.R
+import com.example.movieapp.ui.theme.buttonColor
+import com.example.movieapp.ui.theme.latoFontFamily
+import com.example.movieapp.ui.theme.whiteColor
+
+@Composable
+fun MovieButton(onClick: () -> Unit, isLoading: Boolean,text:String){
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .padding(40.dp)
+            .fillMaxWidth()
+            .height(50.dp)
+            .clip(RoundedCornerShape(6.dp))
+            .background(MaterialTheme.colorScheme.surfaceTint),
+        colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.surfaceTint)
+    ) {
+        if (isLoading) {
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.onPrimary,
+                modifier = Modifier.size(24.dp)
+            )
+        } else {
+            Text(
+                text = text,
+                color = Color.White,
+                fontSize = 18.sp,
+                fontFamily = latoFontFamily
+            )
+        }
+    }
+}
