b92cd0747353064df3284e8e0991ae0d6f8ac5d3
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-08-07 21:40:05 +0300

    Added: Genre Api

---
+++ app/src/main/java/com/example/movieapp/movieList/data/remote/api/response/search_data/Genre.kt
@@ -0,0 +1,6 @@
+package com.example.movieapp.movieList.data.remote.api.response.search_data
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/example/movieapp/movieList/data/remote/api/response/search_data/GenreResult.kt
@@ -0,0 +1,5 @@
+package com.example.movieapp.movieList.data.remote.api.response.search_data
+
+data class GenreResult(
+    val genres: List<Genre>
+)
