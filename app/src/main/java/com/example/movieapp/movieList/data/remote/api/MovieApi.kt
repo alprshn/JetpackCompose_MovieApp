34abcb7f34473389f79dca9eb513bdd3c3d191e9
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-08-08 21:22:00 +0300

    Edit: genreMovieList

--- app/src/main/java/com/example/movieapp/movieList/data/remote/api/MovieApi.kt
+++ app/src/main/java/com/example/movieapp/movieList/data/remote/api/MovieApi.kt
@@ -1,8 +1,11 @@
 package com.example.movieapp.movieList.data.remote.api
 
+import com.example.movieapp.movieList.data.remote.api.response.search_data.GenreResult
+import com.example.movieapp.movieList.data.remote.api.response.search_data.Result
 import com.example.movieapp.movieList.data.remote.api.response.search_data.SearchMovie
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MovieApi {
@@ -21,6 +24,20 @@ interface MovieApi {
         @Query("language") language: String,
         @Header("Authorization") authorization: String
     ): SearchMovie
+
+    @GET("movie/{movie_id}")
+    suspend fun findByIdMovie(
+        @Path("movie_id") movieId: Int,
+        @Query("language") language: String,
+        @Header("Authorization") authorization: String
+    ): Result
+
+    @GET("genre/movie/list")
+    suspend fun genreMovieList(
+        @Query("language") language: String,
+        @Header("Authorization") authorization: String
+    ): GenreResult
+
 
     companion object {
         const val BASE_URL = "https://api.themoviedb.org/3/"
