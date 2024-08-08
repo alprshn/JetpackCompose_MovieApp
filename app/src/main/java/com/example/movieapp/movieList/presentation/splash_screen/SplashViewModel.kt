6f1a060414116afbbc24f0ed3d42b7924d164db1
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-08-08 17:12:02 +0300

    Added: SplashViewModel

---
+++ app/src/main/java/com/example/movieapp/movieList/presentation/splash_screen/SplashViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.movieapp.movieList.presentation.splash_screen
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.movieapp.movieList.domain.repository.AuthenticationRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+
+class SplashViewModel @Inject constructor(private val repository: AuthenticationRepository) :
+    ViewModel() {
+
+
+    private val _isUserAuthenticated = MutableStateFlow(false)
+    val isUserAuthenticated: StateFlow<Boolean> = _isUserAuthenticated.asStateFlow()
+
+    init {
+        checkIfUserIsAuthenticated()
+    }
+
+    private fun checkIfUserIsAuthenticated() = viewModelScope.launch {
+        val isAuthenticated = repository.isUserAuthenticated()
+        Log.d("AuthViewModel", "User is authenticated: $isAuthenticated")
+        _isUserAuthenticated.update { isAuthenticated }
+    }
+}
