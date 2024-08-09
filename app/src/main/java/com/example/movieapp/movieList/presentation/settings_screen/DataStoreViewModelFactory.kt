0f6b77a72e20e02bb8b6bebddf36b1585a111c8c
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-08-09 03:21:00 +0300

    Edit: Added authenticationRepository

--- app/src/main/java/com/example/movieapp/movieList/presentation/settings_screen/DataStoreViewModelFactory.kt
+++ app/src/main/java/com/example/movieapp/movieList/presentation/settings_screen/DataStoreViewModelFactory.kt
@@ -3,12 +3,16 @@ package com.example.movieapp.movieList.presentation.settings_screen
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.movieapp.movieList.data.repository.DataStorePreferenceRepository
+import com.example.movieapp.movieList.domain.repository.AuthenticationRepository
 
-class DataStoreViewModelFactory(private val dataStorePreferenceRepository: DataStorePreferenceRepository):
+class DataStoreViewModelFactory(
+    private val dataStorePreferenceRepository: DataStorePreferenceRepository,
+    private val authenticationRepository: AuthenticationRepository
+) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(SettingsViewModel::class.java)) {
-            return SettingsViewModel(dataStorePreferenceRepository) as T
+            return SettingsViewModel(dataStorePreferenceRepository, authenticationRepository) as T
         }
         throw IllegalStateException()
     }
