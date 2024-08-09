67f05a97ee2bfd4fb723588492516f4c611a898f
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-08-09 03:25:37 +0300

    Edit: Added Preview

--- app/src/main/java/com/example/movieapp/movieList/presentation/components/MovieButton.kt
+++ app/src/main/java/com/example/movieapp/movieList/presentation/components/MovieButton.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.movieapp.R
@@ -49,4 +50,10 @@ fun MovieButton(onClick: () -> Unit, isLoading: Boolean,text:String){
             )
         }
     }
+}
+
+@Preview
+@Composable
+fun MovieButtonPreview(){
+    MovieButton(onClick = {}, isLoading = false, text = stringResource(id = R.string.login))
 }
