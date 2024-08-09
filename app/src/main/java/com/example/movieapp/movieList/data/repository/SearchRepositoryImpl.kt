3a5adc03c05347c400aec3b69c8cbb7093b4e17e
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-08-09 03:23:07 +0300

    Edit: Added findMovieById API and genre API

--- app/src/main/java/com/example/movieapp/movieList/data/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/com/example/movieapp/movieList/data/repository/SearchRepositoryImpl.kt
@@ -4,25 +4,47 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.example.movieapp.movieList.data.remote.api.MovieApi
-import com.example.movieapp.movieList.data.remote.api.response.search_data.Result
+import com.example.movieapp.movieList.data.remote.api.response.Genre
+import com.example.movieapp.movieList.data.remote.api.response.MovieIdResponse
+import com.example.movieapp.movieList.data.remote.api.response.Result
 import com.example.movieapp.movieList.domain.repository.SearchRepository
+import com.example.movieapp.movieList.util.Constants
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class SearchRepositoryImpl @Inject constructor(
     private val movieApi: MovieApi,
 ) : SearchRepository {
-    override fun searchMoviePaging(query: String,language:String): Flow<PagingData<Result>> {
+    override fun searchMoviePaging(query: String, language: String): Flow<PagingData<Result>> {
         return Pager(
             config = PagingConfig(pageSize = 20),
-            pagingSourceFactory = { SearchApiPagingSource(movieApi, query,language) }
+            pagingSourceFactory = { SearchApiPagingSource(movieApi, query, language) }
         ).flow
     }
 
-    override suspend fun popularMoviePaging(language:String): Flow<PagingData<Result>> {
+    override suspend fun popularMoviePaging(language: String): Flow<PagingData<Result>> {
         return Pager(
             config = PagingConfig(pageSize = 20),
-            pagingSourceFactory = { PopularApiPagingSource(movieApi,language) }
+            pagingSourceFactory = { PopularApiPagingSource(movieApi, language) }
         ).flow
+    }
+
+    override suspend fun findMovieById(
+        movieId: Int,
+        language: String
+    ): Flow<MovieIdResponse> {
+        return flow {
+            movieApi.findByIdMovie(movieId, language, Constants.API_KEY)
+                .let { emit(it) }
+        }
+    }
+
+    override suspend fun genreMovieList(language: String): Flow<List<Genre>> {
+        return flow {
+            val genreResponse = movieApi.genreMovieList(language, Constants.API_KEY)
+            emit(genreResponse.genres)
+
+        }
     }
 }
--- app/src/main/java/com/example/movieapp/movieList/domain/repository/SearchRepository.kt
+++ app/src/main/java/com/example/movieapp/movieList/domain/repository/SearchRepository.kt
@@ -1,11 +1,14 @@
 package com.example.movieapp.movieList.domain.repository
 
 import androidx.paging.PagingData
-import com.example.movieapp.movieList.data.remote.api.response.search_data.Result
+import com.example.movieapp.movieList.data.remote.api.response.Genre
+import com.example.movieapp.movieList.data.remote.api.response.MovieIdResponse
+import com.example.movieapp.movieList.data.remote.api.response.Result
 import kotlinx.coroutines.flow.Flow
 
 interface SearchRepository {
     fun searchMoviePaging(query: String, language:String): Flow<PagingData<Result>>
     suspend fun popularMoviePaging(language:String): Flow<PagingData<Result>>
-
+    suspend fun findMovieById(movieId: Int, language:String): Flow<MovieIdResponse>
+    suspend fun genreMovieList(language:String): Flow<List<Genre>>
 }
