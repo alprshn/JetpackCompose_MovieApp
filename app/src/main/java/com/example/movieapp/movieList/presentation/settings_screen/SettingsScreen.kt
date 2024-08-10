9fd9024ac825f0e0db78c1f01218741fa0631fbe
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-08-10 22:33:50 +0300

    Edit: Deleted unnecessary code

--- app/src/main/java/com/example/movieapp/movieList/presentation/settings_screen/SettingsScreen.kt
+++ app/src/main/java/com/example/movieapp/movieList/presentation/settings_screen/SettingsScreen.kt
@@ -359,14 +359,6 @@ fun settingsScreenPreview() {
 }
 
 
-@Composable
-fun SetLanguage(languageCode: String) {
-    val locale = Locale(languageCode)
-    val configuration = LocalConfiguration.current
-    configuration.setLocale(locale)
-    val resources = LocalContext.current.resources
-    resources.updateConfiguration(configuration, resources.displayMetrics)
 
-}
 
 
