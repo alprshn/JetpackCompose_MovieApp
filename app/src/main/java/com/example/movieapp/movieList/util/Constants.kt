20cb1182bd5731a5bd84e5fc5e66ea526d6a3e17
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-08-31 15:10:52 +0300

    Edit: Deleted API KEY

--- app/src/main/java/com/example/movieapp/movieList/util/Constants.kt
+++ app/src/main/java/com/example/movieapp/movieList/util/Constants.kt
@@ -2,5 +2,5 @@ package com.example.movieapp.movieList.util
 
 object Constants {
     const val COLLECTION_NAME_USERS = "users"
-    const val API_KEY = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZGYxNTFjZTFlMGJjNjA3ZTYyZDVlYmEwNTE0YmM1MSIsIm5iZiI6MTcyMDYwNTk5MS44Nzk1NzQsInN1YiI6IjY2OGU1OGY4ZGVlZDMyYzc0NWQ1YjEzMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.x2kiabww_gOlmoSUrTDRiWgESR7eOmrZzi_i9VGuyF8"
+    const val API_KEY = ""
 }
